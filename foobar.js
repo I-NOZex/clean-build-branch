276a2a3d9a3a4fbc770ab79eff8cddb963f9fc91
Tiago Marques <tiagofm.profissional@outlook.pt>
2019-10-10 14:45:51 +0200

    show time

--- foobar.js
+++ foobar.js
@@ -1,18 +1,26 @@
 function foo(){
     let a = 1+1;
+    console.log('1 plus 1')
     return a;
 }
 
 function bar(){
     let b = 1*1;
+    console.log('1 times 1')
     return b;
 }
 
 function foobar(){
     let c = foo() / bar();
+    console.log('foo() / bar()')
     return c;
 }
 
+
 function dummy(){
+    console.info('ei!')
+    console.log('delegate to foobar()')
+    console.error('beep')
+    console.dir('this is redunctant')
     return foobar();
 }
